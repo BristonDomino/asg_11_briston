54b0ab1370e4156c902e4629de38c8c19fde299e
bdddo <Bdomino1@css.edu>
2023-11-19 14:13:52 -0600

    Initial commit

---
+++ android/app/src/main/java/css/cis3334/asg_11_briston/MainActivity.java
@@ -0,0 +1,6 @@
+package css.cis3334.asg_11_briston;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:asg_11_briston/mvc_view.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'My Thing',),
+    );
+  }
+}
